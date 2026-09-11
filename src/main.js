c1acb4d47a7096de836c200c5f6fdf77a408f92b
Aarav Shrivastava <aaravshrivastava4129@gmail.com>
2026-09-11 14:06:34 +0530

    getting images

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 const API_KEY = import.meta.env.VITE_NASA_API_KEY;
-const date = document.querySelector("#datepicker").value;
-fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
+document.querySelector("#app").innerHTML = "<p>loading...</p>";
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`).
+then(response => response.json()).then(data => {
+    document.querySelector("#app").innerHTML = `
+        <h1>${data.title}</h1>
+        <img src="${data.url}" />
+        <p>${data.explanation}</p>
+    `;
+})
