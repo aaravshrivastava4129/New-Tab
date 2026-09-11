06d405b5bbb7b445ce56becd94b5d8e27fb6c8fb
Aarav Shrivastava <aaravshrivastava4129@gmail.com>
2026-09-11 13:44:13 +0530

    add deploy workflow

--- src/main.js
+++ src/main.js
@@ -1,2 +1,3 @@
+const API_KEY = import.meta.env.VITE_NASA_API_KEY;
 const date = document.querySelector("#datepicker").value;
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
